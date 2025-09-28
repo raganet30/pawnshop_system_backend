926c11a4d7599c620ce140b575b4ffcb702bac8a
Kenneth <neth3039@gmail.com>
2025-09-28 13:33:24 +0800

    modified pawn item print

--- processes/pawn_item_print.php
+++ processes/pawn_item_print.php
@@ -50,15 +50,9 @@ $totalRepayment = $pawn['amount_pawned'] + $interest;
 <title>Pawn Ticket</title>
 <style>
     @page {
-    size: auto;   /* let the printer decide paper size */
-    margin: 5mm;  /* safe margin for dot matrix */
-}
-.page {
-    width: 190mm;   /* keeps ticket width inside fanfold */
-    margin: 0 auto;
-}
-
-
+        size: Letter portrait;  /*  default Letter size */
+        margin: 10mm;
+    }
     body {
         font-family: "Courier New", monospace;
         font-size: 10pt;
@@ -66,13 +60,18 @@ $totalRepayment = $pawn['amount_pawned'] + $interest;
         margin: 0;
         padding: 0;
     }
+    .receipt {
+        width: 100%;
+        margin-bottom: 20px;
+        page-break-inside: avoid;
+    }
     table {
         width: 100%;
         border-collapse: collapse;
     }
     td, th {
         padding: 1px 2px;
-        vertical-align: top;    
+        vertical-align: top;
     }
     .border td, .border th {
         border: 1px solid black;
@@ -80,78 +79,87 @@ $totalRepayment = $pawn['amount_pawned'] + $interest;
     }
     .center { text-align: center; }
     .bold { font-weight: bold; }
-    .section { margin-top: 4px; }
     .small { font-size: 9pt; }
-    .page {
-        width: 190mm;   /* keep some space inside margins */
-        margin: 0 auto;
+    .footer-note {
+        text-align: right;
+        margin-top: 10px;
+        font-size: 8pt;
+        font-style: italic;
+    }
+    .cut-line {
+        border-top: 1px dashed #000;
+        margin: 15px 0;
     }
 </style>
 
 </head>
 <body onload="window.print()">
 
-<div class="center bold"><?php echo htmlspecialchars($header['shop_name']); ?></div>
-<div class="center small"><?php echo $_SESSION['user']['branch_name']; ?></div>
-<div class="center small"><?php echo $_SESSION['user']['branch_address']; ?></div>
-<div class="center small">FB Page: <?= htmlspecialchars($header['fb_page_name']); ?></div>
+<?php 
+function renderReceipt($pawn, $header, $totalRepayment, $copyType) { ?>
+    <div class="receipt">
+        <div class="center bold"><?= htmlspecialchars($header['shop_name']); ?></div>
+        <div class="center small"><?= $_SESSION['user']['branch_name']; ?></div>
+        <div class="center small"><?= $_SESSION['user']['branch_address']; ?></div>
+        <div class="center small">FB Page: <?= htmlspecialchars($header['fb_page_name']); ?></div>
+        <br>
 
-<br>
+        <table class="border small">
+            <tr>
+                <td><b>PAWNED DATE:</b> <?= date("m/d/Y", strtotime($pawn['date_pawned'])) ?></td>
+                <td><b>DUE DATE:</b> <?= date("m/d/Y", strtotime($pawn['current_due_date'])) ?></td>
+            </tr>
+            <tr>
+                <td><b>OWNER:</b> <?= strtoupper($pawn['full_name']) ?></td>
+                <td><b>CONTACT #:</b> <?= $pawn['contact_no'] ?></td>
+            </tr>
+            <tr>
+                <td><b>ADDRESS:</b> <?= $pawn['address'] ?></td>
+                <td><b>ITEM:</b> <?= $pawn['unit_description'] ?></td>
+            </tr>
+            <tr>
+                <td><b>REMARKS:</b> <?= $pawn['notes'] ?></td>
+                <td><b>PASSWORD/PIN:</b> <?= $pawn['pass_key'] ?> </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <b>AMOUNT:</b> ₱<?= number_format($pawn['amount_pawned'], 2) ?>&nbsp;&nbsp;
+                    <b>INTEREST:</b> ₱<?= number_format($pawn['amount_pawned'] * $pawn['interest_rate'], 2) ?>&nbsp;&nbsp;
+                    <b>TOTAL REPAYMENT:</b> ₱<?= number_format($totalRepayment, 2) ?>
+                </td>
+            </tr>
+        </table>
 
+        <div class="small">
+            <b>Pawner Declarations:</b><br>
+            1. Nasa aking pang-unawa na kapag hindi ako nakapagbayad ng kabuuang halaga ng ipinahiram na pera kasama ang interes sa loob ng 2 buwan ay magreresulta ng pagkakaremata ng isinanglang gamit.<br>
+            2. Ang battery ng aking gadget ay hindi na responsibilidad ng pawnshop kung sakali mang ito ay masira.<br>
+            3. Ang aking pirma sa ibaba ang pagpapatunay na aking naunawaan ang rules and regulations ng pawnshop. <br><br>
+            Under the penalty of anti-fencing law, dini-deklara ko po na ako ang may-ari ng item na isinanla.
+            <br><br>
+        </div>
 
-<table class="border small">
-    <tr>
-        <td><b>PAWNED DATE:</b> <?= date("m/d/Y", strtotime($pawn['date_pawned'])) ?></td>
-        <td><b>DUE DATE:</b> <?= date("m/d/Y", strtotime($pawn['current_due_date'])) ?></td>
-    </tr>
-    <tr>
-        <td><b>OWNER:</b> <?= strtoupper($pawn['full_name']) ?></td>
-        <td><b>CONTACT #:</b> <?= $pawn['contact_no'] ?></td>
-    </tr>
-    <tr>
-        <td><b>ADDRESS:</b> <?= $pawn['address'] ?></td>
-         <td><b>ITEM:</b> <?= $pawn['unit_description'] ?></td>
-    </tr>
-    
-    <tr>
-        <td><b>REMARKS:</b> <?= $pawn['notes'] ?></td>
-        <td><b>PASSWORD/PIN:</b> <?= $pawn['pass_key'] ?> </td>
-    </tr>
-    <tr>
-        <td colspan="2">
-            <b>AMOUNT:</b> ₱<?= number_format($pawn['amount_pawned'], 2) ?> &nbsp;&nbsp;
-            <b>INTEREST AMOUNT:</b> ₱<?= number_format($pawn['amount_pawned'] * $pawn['interest_rate'], 2) ?> &nbsp;&nbsp;
-            <b>TOTAL REPAYMENT:</b> ₱<?= number_format($totalRepayment, 2) ?>
-        </td>
-    </tr>
-</table>
+        <div class="small">
+            <b>PAWNER:</b> ______________________ &nbsp;&nbsp;
+            <b>CASHIER:</b> ______________________ &nbsp;&nbsp;
+            <b>CLAIMED BY:</b> ____________________
+        </div>
 
+        <div class="small">
+            <b>Business Hours:</b> Mon–Sat (9:00 AM – 6:00 PM)
+        </div>
 
+        <div class="footer-note"><?= $copyType ?> Copy</div>
+    </div>
+<?php } ?>
 
-<div class="section small">
-    <b>Pawner Declarations:</b><br>
-    1. Nasa aking pang-unawa na kapag hindi ako nakapagbayad ng kabuuang halaga ng ipinahiram na pera kasama ang interes sa loob ng 2 buwan ay magreresulta ng pagkakaremata ng isinanglang gamit.<br>
-    2. Ang battery ng aking gadget ay hindi na responsibilidad ng pawnshop kung sakali mang ito ay masira.<br>
-    3. Ang aking pirma sa ibaba ang pagpapatunay na aking naunawaan ang rules and regulations ng pawnshop. <br><br>
-    Under the penalty of anti-fencing law, dini-deklara ko po na ako ang may-ari ng item na isinanla.
-    <br><br>
-   
-</div>
-
-
-<div class="section small">
-    <b>PAWNER:</b> ______________________ &nbsp;&nbsp;
-    <b>CASHIER:</b> ______________________ &nbsp;&nbsp;
-    <b>CLAIMED BY:</b> ____________________
-</div>
-
-<div class="section small">
-    <b>Business Hours:</b> Mon–Sat (9:00 AM – 6:00 PM) <br>
-    <!-- <b>facebook:</b> https://www.facebook.com/ldgadgetpawnshop/ -->
-</div>
-
-
-<br><br>
+<?php 
+// render two copies
+renderReceipt($pawn, $header, $totalRepayment, "Customer's");
+echo '<div class="cut-line"></div>'; // cutting line
+renderReceipt($pawn, $header, $totalRepayment, "Pawnshop's");
+?>
 
 </body>
 </html>
+
